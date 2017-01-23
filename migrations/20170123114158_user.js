01f597bd7e0d34ffd36860ed4cdcaadaea95408d
matt-winzer <matt.winzer@gmail.com>
2017-01-23 12:00:22 -0700

    created migration file for user table

--- migrations/20170123114158_user.js
+++ migrations/20170123114158_user.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('user', function(table){
+    table.increments();
+    table.text('name').notNullable();
+    table.text('email').unique().notNullable();
+    table.text('password').notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('user');
 };
