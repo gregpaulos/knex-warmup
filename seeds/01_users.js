fe5a9d3ca1210203e234e56b1b641441989743c6
matt-winzer <matt.winzer@gmail.com>
2017-01-24 09:27:28 -0700

    seeds include hashed passwords

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -1,3 +1,5 @@
+var bcrypt = require('bcrypt');
+
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex.raw('DELETE FROM "user"; ALTER SEQUENCE user_id_seq RESTART WITH 3;')
@@ -6,12 +8,12 @@ exports.seed = function(knex, Promise) {
         id: 1,
         name: 'sam',
         email: 'sam@gmail.com',
-        password: 'sammyg21',
+        password: bcrypt.hashSync('sammyg21', 10)
       }, {
         id: 2,
         name: 'alex',
         email: 'alex@gmail.com',
-        password: 'alexmart05',
+        password: bcrypt.hashSync('alexmart05', 10)
       }];
       return knex('user').insert(users);
     });
