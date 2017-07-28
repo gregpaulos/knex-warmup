ac47ddfcffc45a935c92ef616009630a76f6fc15
matt-winzer <matt.winzer@gmail.com>
2017-07-28 08:18:45 -0600

    added user routes and validation

--- README.md
+++ README.md
@@ -1,6 +1,45 @@
-# Standards/Objectives
+# Server Side Validation
 
-## Build a CRUD App w/ Single Resource
+## Standards
+
+### Server Side: Validate User Input
+
+- Redirects on success
+- Re-renders on validation error
+- Validates presence of a field
+- Validates format of a field with a regex
+- Validates uniqueness of a field with a query
+
+## Objectives
+
+- Explain what server side validation is
+- Explain why server side validation is important
+- Implement server side validation in Express
+
+## What is Server Side Validation?
+
+Server side validation is the process of a server using a defined set of rules to ensure user input from a client is correct, meaningful, and secure.
+
+The rules to validate on a server can be as strict or lenient as the developer prefers, but remember to never trust user input sent from the client.
+
+## Lesson
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/0iNFgfbJXKQ" frameborder="0" allowfullscreen></iframe>
+
+## Resources
+
+- [Learn Article - Validation](https://learn.galvanize.com/content/gSchool/node-curriculum/master/Express/Validation.md)
+- [Hapijs/Joi - Validation Library](https://github.com/hapijs/joi)
+- [Regexr - Regex Testing & Practice](http://regexr.com/)
+
+
+
+
+# CRUD
+
+## Standards/Objectives
+
+### Build a CRUD App w/ Single Resource
 
 Includes migrations
 Includes seeds
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,109 @@
 var express = require('express');
+var knex = require('../db/knex');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Validations
+
+// Keep bad data out of our database
+
+// Check that input exists
+// Check that input is the correct type
+// Check that input is formatted properly
+
+function validUser(user) {
+  let validName = typeof user.name == 'string' && user.name.trim() != '';
+  let validEmail = typeof user.email == 'string' && user.email.match(/([@])/g) != null;
+  let validPassword = typeof user.password == 'string' && user.password.trim() != '' && user.password.length > 5;
+
+  return validName && validEmail && validPassword;
+}
+
+function validId(req, res, next) {
+  let id = req.params.id;
+
+  if(!isNaN(id)) {
+    return next();
+  } else {
+    res.json({message: 'Invalid ID paramater.'})
+  }
+}
+
+router.get('/', function(req, res) {
+  knex('user')
+    .then(users => {
+      res.json(users)
+    });
 });
+
+router.get('/:id', validId, (req, res) => {
+  let id = req.params.id;
+
+  knex('user')
+    .where('id', id).first()
+    .then(user => {
+      res.json(user);
+    });
+});
+
+router.post('/', (req, res) => {
+  let post = req.body;
+
+  if (validUser(post)) {
+    knex('user').insert(post)
+      .returning('*')
+      .then(user => {
+        res.json(user);
+      });
+  } else {
+    res.json({message: 'Invalid user input.'});
+  }
+
+});
+
+router.put('/:id', (req, res) => {
+  let id = req.params.id;
+  let edit = req.body;
+
+  knex('user').where('id', id)
+    .update(edit)
+    .returning('*')
+    .then(edited => {
+      res.json(edited);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  let id = req.params.id;
+
+  knex('user')
+    .where('id', id).del()
+    .then(deleted => {
+      res.json({
+        message: 'Record Deleted!!',
+        deleted: deleted
+      });
+    });
+});
+
+
+
+
+
+function validUser(user) {
+  const hasName = typeof user.name == 'string' && user.name.trim() != '';
+  const hasEmail = typeof user.email == 'string' && user.email.match(/([@])/g) != null;
+  const hasPassword = typeof user.password == 'string' && user.password.trim() != '';
+  return hasName && hasEmail && hasPassword;
+}
+//
+// function validId(req, res, next) {
+//   let id = req.params.id;
+//
+//   if(!isNaN(id)) {
+//     return next();
+//   } else {
+//     res.json({message: 'Invalid ID'});
+//   }
+// }
 
 module.exports = router;
